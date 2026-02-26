15b74660cfd740da7d8fcfd9dd7d8f6cba19bd69
kphilpot <kphilpot2024@gmail.com>
2026-02-26 17:22:21 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './New_TradeFeed.jsx'
+import App from './New TradeFeed.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
